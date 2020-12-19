a31819bf12c2fe82de87ad62e24bf597571267b4
Rafael Santos <formigarafa@gmail.com>
2020-12-19 21:31:05 +1300

    Update dotfiles_linux_setup.sh

--- dotfiles_linux_setup.sh
+++ dotfiles_linux_setup.sh
@@ -1,20 +1,25 @@
 # NOTE: these instruction may be very outdated
-git clone https://github.com/formigarafa/dotfiles.git ~/.dotfiles
-
 # linux
 apt-get install -y git-core curl build-essential
-apt-get install -y ruby-dev neovim
-apt-get install -y keychain
+apt-get install -y neovim
+apt-get install -y gcc make perl dkms
+apt-get install -y tmux direnv
+git clone https://github.com/formigarafa/dotfiles.git ~/.dotfiles
+
+mkdir /home/raf/.SpaceVim.d
+ln -s /home/raf/.dotfiles/SpaceVim.d/init.toml /home/raf/.SpaceVim.d/
+ln -fs /home/raf/.dotfiles/bashrc /home/raf/.bashrc
+ln -s /home/raf/.dotfiles/gitconfig /home/raf/.gitconfig
+ln -s /home/raf/.dotfiles/tmux.conf /home/raf/.tmux.conf
+ln -s /home/raf/.dotfiles/inputrc /home/raf/.inputrc
+ln -s /home/raf/.dotfiles/pry /home/raf/.pry
+ln -s /home/raf/.dotfiles/pryrc /home/raf/.pryrc
+ln -s /home/raf/.dotfiles/git-prompt-colors.sh /home/raf/.git-prompt-colors.sh
+
+git -C ~/.tmux/plugins/tpm pull || git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+~/.tmux/plugins/tpm/scripts/install_plugins.sh
+
 curl -sLf https://spacevim.org/install.sh | bash
-pip install powerline-shell
-curl -LO https://thoughtbot.github.io/rcm/dist/rcm-1.3.0.tar.gz && \
-\
-sha=$(sha256sum rcm-1.3.0.tar.gz | cut -f1 -d' ') && \
-[ "$sha" = "ddcf638b367b0361d8e063c29fd573dbe1712d1b83e8d5b3a868e4aa45ffc847" ] && \
-\
-tar -xvf rcm-1.3.0.tar.gz && \
-cd rcm-1.3.0 && \
-\
-./configure && \
-make && \
-sudo make install
+git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
+
+curl -o ~/Downloads/Sauce\ Code\ Pro\ Nerd\ Font\ Complete\ Mono.ttf https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/SourceCodePro/Regular/complete/Sauce%20Code%20Pro%20Nerd%20Font%20Complete%20Mono.ttf
